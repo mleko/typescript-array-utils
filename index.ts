7b9c10cfd40b0389d0ae0bfe372431489ce58d3a
Daniel Król <daniel@krol.me>
2017-05-11 10:22:20 +0200

    Add unique

--- index.ts
+++ index.ts
@@ -3,3 +3,4 @@ import "es6-shim";
 export {replace} from "./src/replace";
 export {without} from "./src/without";
 export {move} from "./src/move";
+export {unique} from "./src/unique";
---
+++ src/unique.ts
@@ -0,0 +1,11 @@
+const identityCompare = <T>(a: T, b: T): boolean => {
+	return a === b;
+};
+
+export function unique<T>(arr: T[], compare: (a: T, b: T) => boolean = identityCompare): T[] {
+	return arr.filter((e, index) => {
+		return index === arr.findIndex((v) => {
+			return compare(e, v);
+		})
+	})
+}
